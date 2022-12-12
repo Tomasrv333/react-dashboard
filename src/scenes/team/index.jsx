17bca0e80de4f2a6a241c1c2ca92b19323ac6b54
Tomasrv333 <tomasrv123@gmail.com>
2022-12-12 12:02:55 -0500

    Team scene creating

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -13,13 +14,43 @@ const Team = () => {
 
     const columns = [
         {field: "id", headerName: "ID"},
-        {field: "name", headerName: "Name"},
+        {field: "name", headerName: "Name", flex: 1, cellClassName: "name-column--cell"},
+        {field: "age", headerName: "Age", type: "number", headerAlign: "left", align: "left"},
+        {field: "phone", headerName: "Phone Number", flex: 1},
+        {field: "email", headerName: "Email", flex: 1},
+        {field: "access", headerName: "Access Level", flex: 1, renderCell: ({row:{access}}) => {
+            return (
+                <Box
+                    width="60%"
+                    m="0 auto"
+                    p="5px"
+                    display="flex"
+                    justifyContent="center"
+                    backgroundColor={
+                        access === "admin"
+                            ? colors.greenAccent[600]
+                            : colors.greenAccent[700]
+                    }
+                    borderRadius="4px"
+                >
+                    {access === "admin" && <AdminPanelSettingsOutlined />}
+                    {access === "manager" && <SecurityOutlined />}
+                    {access === "user" && <LockOpenOutlined />}
+                    <Typography color={colors.grey[100]} sx={{nl: "5px"}}>
+                        {access}
+                    </Typography>
+                </Box>
+            )
+        }},
     ]
 
     return (
-        <Box>
+        <Box m="20px">
             <Header title="TEAM" subtitle="Managing the Team Members" />
-            <Box>
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+            >
                 <DataGrid 
                     rows={mockDataTeam}
                     columns={columns}
